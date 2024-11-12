90d9ba101e9ea85e3735e3b22c4c496fea84eda8
Ronmycode <67519456+Ronmycode@users.noreply.github.com>
2024-11-11 23:02:49 -0600

    first push

---
+++ controlStructures.php
@@ -0,0 +1,12 @@
+<?php
+
+#### Creating a Class from stdClass
+/* This is a Generic php empty class to create anything */
+
+$animal = new stdClass();
+$animal -> name = "Ruby";
+$animal -> type = "Dog";
+
+
+
+?>
