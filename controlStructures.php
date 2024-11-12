88d599621833dd4f525ea2aeeead82984319b6f5
Ronmycode <67519456+Ronmycode@users.noreply.github.com>
2024-11-11 23:17:54 -0600

    creating new classes

--- controlStructures.php
+++ controlStructures.php
@@ -1,11 +1,32 @@
 <?php
 
-#### Creating a Class from stdClass
+###################################
+###### Creating clases in PHP #####
+###################################
+
+// OPTION 1 //
+// Creating a Class from stdClass
 /* This is a Generic php empty class to create anything */
 
 $animal = new stdClass();
 $animal -> name = "Ruby";
 $animal -> type = "Dog";
+var_dump($animal);
+
+
+//OPTION 2
+// Creating a Class from scratch
+class student{
+    public $name;
+    public $lastName;
+    public $age;
+}
+$student = new student();
+$student -> name = "Ron";
+$student -> lastName = "MyCode";
+$student -> age = 50;
+
+var_dump($student);
 
 
 
